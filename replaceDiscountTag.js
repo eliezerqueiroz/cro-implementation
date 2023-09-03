459852cd37a3732c1541ddac62025edeac8b4e71
Eliezer Queiroz <eliezer.lima@betrybe.com>
2023-09-02 21:25:05 -0300

    clone buyBtn and add blue background

--- replaceDiscountTag.js
+++ replaceDiscountTag.js
@@ -5,13 +5,18 @@
     const discountTag = document.querySelector('[data-testid="price-default"] > span');
     const bagButton = document.querySelector('[data-testid="bagButton"]');
     const buyButton = document.querySelector('[data-testid="buyButton"]');
+    const cloneButton = buyButton.cloneNode(true);
     const floatTag = document.querySelector('[data-testid="showcase-price"] small');
+    const floatButton = document.querySelector('[data-testid="float-button"]');
     const discountPrice = priceOriginal - pixPrice;
     
+    // bagButton.style.visibility = "hidden";
     bagButton.style.display = "none";
     buyButton.style.backgroundColor = "rgba(0,134,255,255)";
-    // bagButton.style.visibility = "hidden";
+    cloneButton.style.backgroundColor = "rgba(0,134,255,255)";
+    cloneButton.addEventListener("click", () => buyButton.click());
     discountTag.innerHTML = `(${discountPrice.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })} de <strong>desconto</strong>)`;
+    floatButton.parentNode.replaceChild(cloneButton, floatButton);
     if(floatTag !== null){
         floatTag.classList = discountTag.className;
         floatTag.innerHTML = discountTag.innerHTML;
